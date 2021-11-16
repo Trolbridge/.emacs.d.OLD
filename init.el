71647204dc86c0a94d2e1cfec75a336c7e3bdd54
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2021-11-15 22:58:31 -0500

    Major modication to the Emacs.org file to now config init.el

--- init.el
+++ init.el
@@ -1,17 +1,29 @@
-;; This is not needed for Emacs Plus
-;; https://github.com/d12frosted/homebrew-emacs-plus
-;; For GitHub Org-Mode:
-;; (add-to-list 'load-path "~/.emacs.d/src/org-mode/lisp")
-;; (require 'org-loaddefs)
+;; Initialize package sources
 
-;;(setq org-agenda-start-day "-1d")
-(setq org-agenda-span 16)
-(setq org-agenda-start-on-weekday nil)
-(setq org-image-actual-width nil)
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")
+;;			 ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (setq inhibit-startup-message t)
 (setq visible-bell t)
-(set-face-attribute 'default nil :font "Source Code Pro" :height 140)
 
 (scroll-bar-mode -1)	; Disable visible scrollbar
 (tool-bar-mode -1)	; Disable the toolbar
@@ -36,48 +48,70 @@
         (t (self-insert-command (or arg 1)))))
 
 ;;---------------------------------------------------------------------------------
+
+(set-face-attribute 'default nil :font "Source Code Pro" :height 140)
+
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-;; Initialize package sources
+(use-package general
+  :config
+  (general-create-definer trol/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
 
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+  (trol/leader-keys
+    "t" '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")))
 
+(general-define-key
+ "C-M-j" 'counsel-switch-buffer)
+ ;"M-x" 'amx
+ ;"C-s" 'counsel-grep-or-swiper)
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("melpa-stable" . "https://stable.melpa.org/packages/")
-;;			 ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+;(defun trol/evil-hook ()
+ ; (dolist (mode '(custom-mode
+;		  eshell-mode
+;		  git-rebase-mode
+;		  erc-mode
+;		  circe-server-mode
+;		  circe-chat-mode
+;		  circe-query-mode
+;		  sauron-mode
+;		  term-mode))
+;    (add-to-list 'evil-emacs-state-modes mode)))
 
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+;; Access icloud:
+;; ~/Library/Mobile\ Documents/com~apple~CloudDocs/
 
-;; Initialize use-package on non-linux platforms
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+;  :hook (evil-mode . trol/evil-hook)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-;") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
-(require 'use-package)
-(setq use-package-always-ensure t)
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
 
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(htmlize visual-fill-column visual-fill-mode magit counsel-projectile projectile prjectile hydra evil-collection evil general all-the-icons-dired helpful counsel-world-clock counsel ivy-rich which-key rainbow-delimiters doom-modeline use-package-hydra ivy command-log-mode use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
 
 (use-package command-log-mode)
+
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
@@ -101,7 +135,26 @@
 ;; (define-key emacs-lisp-mode-map (kbd "C-x M-t") 'counsel-load-theme)
 
 
-(use-package doom-modeline
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; Don't start start with ^
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+  (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 5)))
@@ -135,35 +188,23 @@
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
 
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-         ("C-x b" . counsel-ibuffer)
-         ("C-x C-f" . counsel-find-file)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history))
-  :config
-  (setq ivy-initial-inputs-alist nil)) ;; Don't start start with ^
-
-(use-package ivy-rich
-  :init
-  (ivy-rich-mode 1))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(htmlize visual-fill-column visual-fill-mode magit counsel-projectile projectile prjectile hydra evil-collection evil general all-the-icons-dired helpful counsel-world-clock counsel ivy-rich which-key rainbow-delimiters doom-modeline use-package-hydra ivy command-log-mode use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 
-(use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
+
 
 (use-package all-the-icons
   :if (display-graphic-p)
@@ -176,61 +217,15 @@
   :if (display-graphic-p)
   :hook (dired-mode . all-the-icons-dired-mode))
 
-(use-package general
-  :config
-  (general-create-definer trol/leader-keys
-    :keymaps '(normal insert visual emacs)
-    :prefix "SPC"
-    :global-prefix "C-SPC")
-
-  (trol/leader-keys
-    "t" '(:ignore t :which-key "toggles")
-    "tt" '(counsel-load-theme :which-key "choose theme")))
-
-
-
-
-(general-define-key
- "C-M-j" 'counsel-switch-buffer)
- ;"M-x" 'amx
- ;"C-s" 'counsel-grep-or-swiper)
-
-;(defun trol/evil-hook ()
- ; (dolist (mode '(custom-mode
-;		  eshell-mode
-;		  git-rebase-mode
-;		  erc-mode
-;		  circe-server-mode
-;		  circe-chat-mode
-;		  circe-query-mode
-;		  sauron-mode
-;		  term-mode))
-;    (add-to-list 'evil-emacs-state-modes mode)))
-
-(use-package evil
-  :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
-;  :hook (evil-mode . trol/evil-hook)
-  :config
-  (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-;") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-
-  ;; Use visual line motions even outside of visual-line-mode buffers
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-
-  (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))
-
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
 
 (use-package hydra)
 
@@ -243,33 +238,15 @@
 (trol/leader-keys
   "ts" '(hydra-text-scale/body :which-key "scale text"))
 
-;; Access icloud:
-;; ~/Library/Mobile\ Documents/com~apple~CloudDocs/
-
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  (when (file-directory-p "~/Library/Mobile Documents/com~apple~CloudDocs/Emacs/Projects/Code")
-   (setq projectile-project-search-path '("~/Library/Mobile Documents/com~apple~CloudDocs/Emacs/Projects/Code")))
-  (setq projectile-switch-project-action #'projectile-dired))
-
-;; Set M-o Options:
-(use-package counsel-projectile
-  :config (counsel-projectile-mode))
-
-(use-package magit)
-;;  :custom
-;;  (magit-display-buffer-function #'magit-display-buffer-same-window-execept-dff-v1))
+;;(setq org-agenda-start-day "-1d")
+(setq org-agenda-span 16)
+(setq org-agenda-start-on-weekday nil)
+(setq org-image-actual-width nil)
 
 (defun trol/org-mode-setup ()
   (org-indent-mode)
   (variable-pitch-mode 1)
   (visual-line-mode 1))
-
 
 (use-package org
   :hook (org-mode . trol/org-mode-setup) 
@@ -449,14 +426,17 @@
   :defer t
   :hook (org-mode . trol/org-mode-visual-fill))
 
-(use-package htmlize)
-
 (require 'ob-js)
 (org-babel-do-load-languages
-  'org-babel-load-languages
-  '((emacs-lisp . t)
-    (js . t)
-    (python . t)))
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (js . t)
+   (python . t)))
+
+
+(push '("conf-unix" . conf-unix) org-src-lang-modes)
+
+
 
 ;; This is needed as of Org 9.2
 (require 'org-tempo)
@@ -467,3 +447,36 @@
 (add-to-list 'org-structure-template-alist '("js" . "src js :results output"))
 
 (setq org-confirm-babel-evaluate nil)
+
+;; Automatically tangle our Emacs.org config file when we save it
+(defun trol/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+                      (expand-file-name "~/.emacs.d/OrgFiles/Emacs.org"))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'trol/org-babel-tangle-config)))
+
+(use-package htmlize)
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/Library/Mobile Documents/com~apple~CloudDocs/Emacs/Projects/Code")
+   (setq projectile-project-search-path '("~/Library/Mobile Documents/com~apple~CloudDocs/Emacs/Projects/Code")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+;; Set M-o Options:
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package magit)
+;;  :custom
+;;  (magit-display-buffer-function #'magit-display-buffer-same-window-execept-dff-v1))
+
+;;  (+ 55 100)

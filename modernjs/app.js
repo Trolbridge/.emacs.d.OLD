5510b7b71bd9749104990bdb63a8ecdd545931c1
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2021-11-20 01:59:27 -0500

    Adjusted Arrys, Objects and Tables

--- modernjs/app.js
+++ modernjs/app.js
@@ -1,4 +1,10 @@
 
+ console.log([1,2,3,4]);   // Arrays
+ console.log({a:1,b:2});   // Object Literal
+ console.table({a:1,b:2}); // Tables
+ console.error('This is an error');  // Errors
+ console.warn('This is an warning');  // Warnings
+
 
 
 

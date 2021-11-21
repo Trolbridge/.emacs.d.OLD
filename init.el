cb99c827f415dca4d9c8f800326c6a170767418a
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2021-11-20 22:17:08 -0500

    Started Emacs Lisp Series

--- init.el
+++ init.el
@@ -484,7 +484,7 @@
   (lsp-headerline-breadcrumb-mode))
 
 (use-package lsp-mode
-  :commands (lsp lsp-deferred)
+  ;; :commands (lsp lsp-deferred)
   ;; :commands (lsp lsp-deferred)
   :hook (lsp-mode . trol/lsp-mode-setup)
   :init
@@ -552,18 +552,22 @@
   (setq typescript-indent-level 2))
 
 (use-package company
+
   :after lsp-mode
   :hook (lsp-mode . company-mode)
   :bind (:map company-active-map
-         ("<tab>" . company-complete-selection))
-        (:map lsp-mode-map
-         ("<tab>" . company-indent-or-complete-common))
+              ("<tab>" . company-complete-selection))
+            (:map lsp-mode-map
+             ("<tab>" . company-indent-or-complete-common))
   :custom
   (company-minimum-prefix-length 1)
   (company-idle-delay 0.0))
 
-(use-package company-box
-  :hook (company-mode . company-box-mode))
+
+(add-hook 'after-init-hook 'global-company-mode)
+
+    (use-package company-box
+      :hook (company-mode . company-box-mode))
 
 (use-package projectile
   :diminish projectile-mode
--- modernjs/009typeConversion.js
+++ modernjs/009typeConversion.js
@@ -1,5 +1,6 @@
 let val;
 
+
 val=5;
 
 

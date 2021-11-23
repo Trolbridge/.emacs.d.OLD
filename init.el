ca42b251a82ad2ad1c0a3a7e1347420179935503
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2021-11-23 12:31:59 -0500

    Added but commented-out Relative line numbers

--- init.el
+++ init.el
@@ -627,3 +627,21 @@
 (use-package magit)
 ;;  :custom
 ;;  (magit-display-buffer-function #'magit-display-buffer-same-window-execept-dff-v1))
+
+;;  (defvar my-linum-current-line-number 0)
+
+;; (setq linum-format 'my-linum-relative-line-numbers)
+
+;; (defun my-linum-relative-line-numbers (line-number)
+;;   (let ((test2 (- line-number my-linum-current-line-number)))
+;;     (propertize
+;;      (number-to-string (cond ((<= test2 0) (* -1 test2))
+;;                              ((> test2 0) test2)))
+;;      'face 'linum)))
+
+;; (defadvice linum-update (around my-linum-update)
+;;   (let ((my-linum-current-line-number (line-number-at-pos)))
+;;     ad-do-it))
+;; (ad-activate 'linum-update)
+
+;; (global-linum-mode t)

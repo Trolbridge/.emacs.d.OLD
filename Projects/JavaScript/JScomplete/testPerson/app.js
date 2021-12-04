6585fc307bccd0d9147750e7e711bdbbf435d733
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2021-12-04 14:24:20 -0500

    Object Creation, testapp....

--- Projects/JavaScript/JScomplete/testPerson/app.js
+++ Projects/JavaScript/JScomplete/testPerson/app.js
@@ -3,3 +3,33 @@ console.log(dog);
 
 dog.name = "Fluffy";
 console.log(dog);
+
+
+// Dog Object Prototype
+
+dogProt = {
+    woof: function() {
+        console.log(this.sound)
+    }
+};
+
+let doggy = Object.create(dogProt);
+console.log(doggy);
+console.log(doggy.woof());
+
+doggy.sound = 'Ruff';
+console.log(doggy.woof());
+doggy.woof();
+
+// with Optional parameters
+
+let dog2 = Object.create(dogProt, {
+    'sound': {
+        value: 'bow wow'
+    }
+
+});
+
+dog2.woof();
+
+// console.log(dog2.woof());

c5846319333c01ea34bc294078bc3aa89e07fa16
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2021-12-25 16:13:19 -0500

    Update gc2

--- Projects/Go/main.go
+++ Projects/Go/main.go
@@ -6,62 +6,23 @@ import (
 	"runtime"
 )
 
-func check() {
-	// _, err := exec.Command("cd", "/Users/trolbridge/.emacs.d").Output()
-
-	// if err != nil {
-	// 	fmt.Printf("%s", err)
-	// }
-
-	// out, err := exec.Command("pwd").Output()
-
-	// if err != nil {
-	// 	fmt.Printf("%s", err)
-	// }
-	// output := string(out[:])
-	// fmt.Printf("Checking %s", output)
-
-	out2, err := exec.Command("/Users/trolbridge/.emacs.d/", "git", "status").Output()
-	if err != nil {
-		fmt.Printf("%s", err)
+func main() {
+	if runtime.GOOS == "windows" {
+		fmt.Println("Can't Execute this on a windows machine")
+	} else {
+		execute()
 	}
-	output2 := string(out2[:])
-	fmt.Println(output2)
-
-}
-func check2() {
-	// fmt.Println("Checking ~/OrgRoam")
-	out, err := exec.Command("cd", "/Users/trolbridge/.emacs.d").Output()
-
-	if err != nil {
-		fmt.Printf("%s", err)
-	}
-	output := string(out[:])
-	fmt.Printf("Checking %s", output)
-
-	out2, err := exec.Command("pwd").Output()
-	// out2, err := exec.Command("git", "status").Output()
-	if err != nil {
-		fmt.Printf("%s", err)
-	}
-
-	// fmt.Println("Command Successfully Executed")
-	output2 := string(out2[:])
-	fmt.Println(output2)
 }
 
-func check3() {
-	cmd := exec.Command("git", "status")
-	cmd.Dir = "/Users/trolbridge/.emacs.d"
-	out, err := cmd.Output()
-	if err != nil {
-		fmt.Printf("%s", err)
-	}
-	output := string(out[:])
-	fmt.Println(output)
+func execute() {
+	var myDir string = "/Users/trolbridge/.emacs.d"
+	fmt.Printf(check(&myDir))
+	myDir = "/Users/trolbridge/OrgRoam"
+	fmt.Printf(check(&myDir) + "\n")
+
 }
 
-func check4(myDir *string) string {
+func check(myDir *string) string {
 	cmd := exec.Command("git", "status")
 	cmd.Dir = *myDir
 	out, err := cmd.Output()
@@ -74,37 +35,10 @@ func check4(myDir *string) string {
 
 	var myResult string = ""
 	if myTest == output {
-		// fmt.Println("Clean")
-		// myResult = "Clean"
-		// fmt.Printf("\n" + *myDir + " - ")
 		myResult = ""
 	} else if myTest != output {
-		// fmt.Println("Updated...")
 		fmt.Printf("\n" + *myDir + " - ")
 		myResult = "Updated"
-		// fmt.Println(output)
 	}
-	// imDone := "Im done\n"
-	// return imDone
 	return myResult
-}
-
-func execute() {
-	// check()
-	// check2()
-	// check3()
-	var myDir string = "/Users/trolbridge/.emacs.d"
-	fmt.Printf(check4(&myDir))
-	// check4(&myDir)
-	myDir = "/Users/trolbridge/OrgRoam"
-	fmt.Printf(check4(&myDir) + "\n")
-
-}
-
-func main() {
-	if runtime.GOOS == "windows" {
-		fmt.Println("Can't Execute this on a windows machine")
-	} else {
-		execute()
-	}
 }

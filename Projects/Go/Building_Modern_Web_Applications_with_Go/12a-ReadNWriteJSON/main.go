f49ab69245298134271d4d0466455863dd546d9d
Trolbridge <trolbridge.x@gmail.com>
2021-12-27 17:37:57 -0500

    Go Channels and Packages

---
+++ Projects/Go/Building_Modern_Web_Applications_with_Go/12a-ReadNWriteJSON/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	myJson := `
+[
+    {
+        "first_name": "Clark",
+        "last_name": "Kent",
+        "hair_color": "black",
+        "has_dog": true
+    },
+    {
+        "first_name": "Bruce",
+        "last_name": "Wayne",
+        "hair_color": "black",
+        "has_dog": false
+    }
+]`
+}

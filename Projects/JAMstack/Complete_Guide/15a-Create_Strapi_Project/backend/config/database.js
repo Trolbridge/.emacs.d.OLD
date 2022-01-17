73c78bdd10c9672e0e592f906d84bfe12bf44398
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-17 11:18:14 -0500

    15a-Create_strapi - redo macair

--- Projects/JAMstack/Complete_Guide/15a-Create_Strapi_Project/backend/config/database.js
+++ Projects/JAMstack/Complete_Guide/15a-Create_Strapi_Project/backend/config/database.js
@@ -4,12 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'Strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
+        uri: env('DATABASE_URI'),
+        // host: env('DATABASE_HOST', '127.0.0.1'),
+        // srv: env.bool('DATABASE_SRV', false),
+        // port: env.int('DATABASE_PORT', 27017),
+        // database: env('DATABASE_NAME', 'Strapi'),
+        // username: env('DATABASE_USERNAME', ''),
+        // password: env('DATABASE_PASSWORD', ''),
       },
       options: {
         ssl: true,
---
+++ Projects/JAMstack/Complete_Guide/15a-Create_Strapi_Project/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3388e793-f529-4356-8736-03e0033b8142'
+};

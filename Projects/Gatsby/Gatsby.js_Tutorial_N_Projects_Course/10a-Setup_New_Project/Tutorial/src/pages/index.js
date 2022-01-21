697659b363737f5ee23583885d5ae3ae511c12f5
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-21 10:59:22 -0500

    Gatsby 16 - Navbar

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
+import Navbar from "../components/Navbar"
 
 export default function Home() {
   return (
     <div>
-      <h1>rafce Hello People!</h1>
+      <Navbar />
+      <h1>Hello People!</h1>
       <div>
         <Link to="/company/history">history</Link>
       </div>

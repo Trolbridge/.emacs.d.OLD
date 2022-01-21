636ac421db51b4d5ef8e3a58ab57a20a426f8901
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-21 18:28:53 -0500

    First Deploy 2

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/gatsby-config.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/gatsby-config.js
@@ -5,8 +5,9 @@
  */
 
 module.exports = {
-  pathPrefix:
-    "/.emacs.d/Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/tutorial",
+  pathPrefix: "/.emacs.d",
+  // pathPrefix:
+  //   "/.emacs.d/Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/tutorial",
   /* Your site config here */
   plugins: [],
 }

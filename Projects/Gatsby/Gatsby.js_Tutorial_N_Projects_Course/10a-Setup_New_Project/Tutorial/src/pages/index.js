7ebd6bdf3668b5b76f2d2bd2a674a5577e8a4930
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-21 12:07:42 -0500

    Gatsby 18 - Inline CSS

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/Layout"
 export default function Home() {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <h1 style={{ color: "gold", textTransform: "capitalize" }}>home page</h1>
       <div>
         <Link to="/company/history">history</Link>
       </div>

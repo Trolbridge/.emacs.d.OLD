0e6d3981fd0e1cd89b417d38a2b2515fc11d9c26
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-21 10:34:04 -0500

    Gatsby 15 - Links

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <h1>rafce Hello People!</h1>
+  return (
+    <div>
+      <h1>rafce Hello People!</h1>
+      <div>
+        <Link to="/company/history">history</Link>
+      </div>
+      <div>
+        <Link to="/about">about</Link>
+      </div>
+      <a href="/about">reg link</a>
+      <a href="https://www.gatsbyjs.com/docs/">gatsby docs</a>
+    </div>
+  )
 }

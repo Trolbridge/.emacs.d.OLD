595f431821576080887c0962d92c5bc9c20e532d
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-22 17:43:17 -0500

    Start Cleanup

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
@@ -1,28 +1,10 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { page, text } from "../examples/home.module.css"
-// import * as homeStyles from "../examples/home.module.css"
-import { ExampleButton } from "../examples/button"
 
 export default function Home() {
   return (
     <Layout>
-      {/* <h1 style={{ color: "gold", textTransform: "capitalize" }}>home page</h1> */}
-      <div className={page}>
-        <ExampleButton>click me</ExampleButton>
-        {/* <div className={homeStyles.page}> */}
-        <h1>home page</h1>
-        <h1>hello world</h1>
-        <h1>hello people</h1>
-        <h1>Hi Den!</h1>
-        <h1>Yo BB, you ready to eat???</h1>
-        <p className={text}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
-          vitae, voluptatem, tempore non deleniti, ducimus maxime amet et omnis
-          laboriosam nihil eligendi cupiditate nemo natus? Quia ipsum quis
-          accusantium voluptatibus?
-        </p>
-      </div>
+      <h1>home page</h1>
     </Layout>
   )
 }

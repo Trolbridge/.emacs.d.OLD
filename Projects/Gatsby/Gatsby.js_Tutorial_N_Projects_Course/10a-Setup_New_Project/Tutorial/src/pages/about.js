d60c41b485cc0ce4941d7966a42df67b67a0b3aa
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-22 14:34:00 -0500

    CSS Modules

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/about.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/about.js
@@ -1,11 +1,19 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { page, text } from "../examples/about.module.css"
 
 const about = () => {
   return (
     <Layout>
-      <div className="about-heading">
+      <div className={page}>
         <h1>About Page</h1>
+        <h1 className={text}>hello world</h1>
+        <p className={text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero quas
+          atque facilis, amet exercitationem, dolorum minima distinctio sapiente
+          unde ipsam accusantium aut omnis accusamus quaerat aliquid praesentium
+          molestias laborum? Eveniet.
+        </p>
       </div>
     </Layout>
   )
--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
@@ -1,14 +1,24 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { page, text } from "../examples/home.module.css"
+// import * as homeStyles from "../examples/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
       {/* <h1 style={{ color: "gold", textTransform: "capitalize" }}>home page</h1> */}
-      <div className="home-heading">
+      <div className={page}>
+        {/* <div className={homeStyles.page}> */}
         <h1>home page</h1>
         <h1>hello world</h1>
         <h1>hello people</h1>
+        <h1>Hi Den!</h1>
+        <p className={text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
+          vitae, voluptatem, tempore non deleniti, ducimus maxime amet et omnis
+          laboriosam nihil eligendi cupiditate nemo natus? Quia ipsum quis
+          accusantium voluptatibus?
+        </p>
       </div>
     </Layout>
   )

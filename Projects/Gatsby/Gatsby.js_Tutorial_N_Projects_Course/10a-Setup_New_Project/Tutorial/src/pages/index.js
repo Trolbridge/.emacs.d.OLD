1bebe5c3fa6d247c876905f169b5f5d490b6a115
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-22 09:06:14 -0500

    Update Layout

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
@@ -6,14 +6,7 @@ export default function Home() {
   return (
     <Layout>
       <h1 style={{ color: "gold", textTransform: "capitalize" }}>home page</h1>
-      <div>
-        <Link to="/company/history">history</Link>
-      </div>
-      <div>
-        <Link to="/about">about</Link>
-      </div>
-      <a href="/about">reg link</a>
-      <a href="https://www.gatsbyjs.com/docs/">gatsby docs</a>
+      <h1>hello world</h1>
     </Layout>
   )
 }

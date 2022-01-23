5a1bf614af47d7de964a2cf4e3ef073eea211436
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-22 20:07:39 -0500

    Footer pt 1

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/components/Footer.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/components/Footer.js
@@ -2,8 +2,11 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <footer>
-      <h2>Footer</h2>
+    <footer className="page-footer">
+      <p>
+        &copy; {new Date().getFullYear()}
+        <span>SimplyRecipes</span>
+      </p>
     </footer>
   )
 }

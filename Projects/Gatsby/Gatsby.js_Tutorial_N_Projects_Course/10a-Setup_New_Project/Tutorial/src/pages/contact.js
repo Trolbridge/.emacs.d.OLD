0eb0618ce948cbfa80489ac77fd4964b5a2651b5
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-23 10:07:31 -0500

    Contact Page 2

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/contact.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/contact.js
@@ -10,31 +10,13 @@ const Contact = () => {
           <article className="contact info">
             <p>
               Forage bushwick blue bottle, four dollar toast put a bird on it
-              succulents ramps organic woke chia glossier etsy aute. Lyft
-              bushwick fingerstache chambray truffaut, lumbersexual trust fund
-              tilde narwhal. Elit quinoa ipsum, fixie pinterest pabst selvage.
-              Ut aliqua woke, proident adaptogen plaid salvia sunt.
+              succulents ramps organic woke chia glossier etsy aute.
             </p>
-            <p>
-              Palo santo lumbersexual bitters, bespoke biodiesel keytar ethical
-              food truck flexitarian deserunt. Aliquip gochujang typewriter,
-              chartreuse synth pariatur ugh chillwave aute yr dolore microdosing
-              est non. Consectetur 3 wolf moon la croix seitan blog cronut
-              voluptate, chillwave YOLO. Minim eiusmod fugiat, ramps man bun
-              skateboard duis nisi street art irony fashion axe in.
-            </p>
+            <p>Palo street art irony fashion axe in.</p>
             <p>
               Cupidatat forage pitchfork, whatever selvage glossier portland you
               probably haven't heard of them viral tousled. Stumptown crucifix
               fashion axe succulents voluptate, air plant mollit swag.
-              Williamsburg brooklyn crucifix cray brunch consequat church-key
-              iPhone ad gochujang intelligentsia paleo gastropub aliquip.
-              Consectetur bitters banh mi duis lorem, in keffiyeh magna squid.
-              Unicorn street art incididunt, hashtag actually mixtape air plant
-              normcore gastropub vinyl mustache banjo shoreditch. Next level
-              shabby chic freegan gastropub live-edge dolore, migas health goth
-              etsy pok pok distillery bespoke taiyaki. Ad 90's voluptate,
-              pinterest shoreditch tumeric salvia.
             </p>
           </article>
           <article>

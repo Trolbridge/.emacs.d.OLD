e979d7110a21599f73f1aa8c44ea072eb4c0355d
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-25 18:52:34 -0500

    MacAir Sandbox Setup

---
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/examples/Images.js
@@ -0,0 +1,13 @@
+import React from "react"
+import styled from "styled-components"
+
+const Images = () => {
+  return (
+    <Wrapper>
+      <h2>Gatsby Wrapper</h2>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section
+export default Images

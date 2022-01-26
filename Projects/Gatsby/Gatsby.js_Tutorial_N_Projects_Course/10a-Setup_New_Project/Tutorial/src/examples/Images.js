15d088513e2a48a8945a19f5d10f4486fbec381a
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-25 22:43:18 -0500

    Setup sandbox for image testing 2

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/examples/Images.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/examples/Images.js
@@ -2,18 +2,21 @@ import React from "react"
 import styled from "styled-components"
 import sunset from "../assets/images/sunset.jpeg"
 // import big from "../assets/images/big.jpg"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Images = () => {
   return (
     <Wrapper>
-      <h2>Gatsby Image</h2>
-      <img src={sunset} alt="sunset" />
+      <h2>Image Test</h2>
+      {/* <img src={sunset} alt="sunset" /> */}
+      <StaticImage src="../assets/images/sunset.jpeg"></StaticImage>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
-img{
-  width: 400px;
-}`
+  img {
+    width: 400px;
+  }
+`
 export default Images

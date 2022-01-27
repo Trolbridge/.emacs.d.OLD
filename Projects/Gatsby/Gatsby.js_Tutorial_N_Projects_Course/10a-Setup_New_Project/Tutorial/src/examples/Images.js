e13ee7767b6d18cac5fd34106291af029626f163
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-26 21:49:24 -0500

    38 Options Example

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/examples/Images.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/examples/Images.js
@@ -1,26 +1,34 @@
 import React from "react"
 import styled from "styled-components"
-import sunset from "../assets/images/sunset.jpeg"
-// import big from "../assets/images/big.jpg"
 import { StaticImage } from "gatsby-plugin-image"
 
 const Images = () => {
   return (
     <Wrapper>
-      <h2>Image Test</h2>
-      {/* <img src={sunset} alt="sunset" /> */}
-      <StaticImage src="../assets/images/sunset.jpeg" alt="sunset" />
-      <StaticImage
-        src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1613666858/course%20slides/event-loop_gohdkk.png"
-        alt="code"
-      />
+      <article>
+        <h4>constrained / default</h4>
+
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          height={400}
+          placeholder="tracedSVG"
+          layout="constrained"
+          className="example-img"
+          as="section"
+        />
+      </article>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
-  img {
-    width: 400px;
-  }
+width: 70vw;
+margin: 0 auto;
+article{
+  border: 2px solid red;
+}.example-img{
+  border-radius:1rem;
+}
 `
 export default Images
--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import Images from "../examples/Images"
+import big from "../assets/images/big.jpg"
 
 export default function Home() {
   return (

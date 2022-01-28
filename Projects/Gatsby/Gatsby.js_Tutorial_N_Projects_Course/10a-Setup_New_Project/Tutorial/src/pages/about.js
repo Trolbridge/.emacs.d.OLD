b8e70d251c8e470138b3bf9fee9cfab0890f1fea
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-28 17:35:33 -0500

    41 About Page

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/about.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/about.js
@@ -1,12 +1,37 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
-const about = () => {
+const About = () => {
   return (
     <Layout>
-      <h1>About Page</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+              odit impedit quam ipsum culpa sint vitae, voluptas distinctio
+              tempora.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="person pouring salt in bowl"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
 
-export default about
+export default About
--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
@@ -1,14 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import Images from "../examples/Images"
-import big from "../assets/images/big.jpg"
+// import Images from "../examples/Images"
+// import big from "../assets/images/big.jpg"
 
 export default function Home() {
   return (
     <Layout>
       <h1>Home page</h1>
-      <Images />
+      {/* <Images /> */}
     </Layout>
   )
 }

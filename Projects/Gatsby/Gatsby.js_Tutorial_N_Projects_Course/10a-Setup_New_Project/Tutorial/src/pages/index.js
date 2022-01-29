b5f1036c61af9ad27d5ad82defa6989013c1b1a2
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-01-28 19:19:42 -0500

    42 Deploy Hero Page, watch that sneaky W

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 export default function Home() {
   return (
@@ -12,8 +13,14 @@ export default function Home() {
             alt="eggs"
             className="hero-img"
             placeholder="blurred"
-            layout="fullwidth"
-          ></StaticImage>
+            layout="fullWidth"
+          />
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>Simply Recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div>
         </header>
       </main>
     </Layout>

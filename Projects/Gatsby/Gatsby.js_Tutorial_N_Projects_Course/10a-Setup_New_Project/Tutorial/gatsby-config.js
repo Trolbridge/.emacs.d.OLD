980ba2c4e66b336f1fee604b5a0e60c8e54fcaca
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-02-26 22:57:08 -0500

    54 Install SOURCE-FILESYSTEM Plugin

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/gatsby-config.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/gatsby-config.js
@@ -11,8 +11,9 @@ module.exports = {
     description: "Nice and clean recipes site",
     author: "@dennismitchell",
     person: { name: "dennis", age: 53 }, // Object
-    simpleData: ["item 1", "item 2"],     // Array
-    complexData: [                        // Array of Objects
+    simpleData: ["item 1", "item 2"], // Array
+    complexData: [
+      // Array of Objects
       { name: "dennis", age: 53 },
       { name: "susan", age: 52 },
     ],
@@ -22,5 +23,12 @@ module.exports = {
     `gatsby-plugin-image`, // gatsby-plugin-image
     `gatsby-plugin-sharp`, // gatsby-plugin-image
     `gatsby-transformer-sharp`, // gatsby-plugin-image - Needed for dynamic images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
   ],
 }

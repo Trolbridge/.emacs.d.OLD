92c9d0a85bf3364ac47d9d049e6624dcefbd0da1
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-02-27 19:26:01 -0500

    56 Just Query Demo

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/pages/index.js
@@ -1,7 +1,8 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
-import FetchData from '../examples/fetchData'
+import FetchData from "../examples/fetchData"
+// Lesson 56 Query Stuff
 
 export default function Home() {
   return (

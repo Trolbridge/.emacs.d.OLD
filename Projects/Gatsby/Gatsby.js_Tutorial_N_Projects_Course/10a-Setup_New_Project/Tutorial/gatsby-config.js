ee92de45afa1b211957c0948bd7ded1bce891432
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-03-02 15:17:10 -0500

    72. ENV Variables

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/gatsby-config.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/gatsby-config.js
@@ -4,6 +4,13 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+/* hi */
+// require("events").EventEmitter.defaultMaxListeners = 15
+
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -28,6 +35,14 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ],

486c4ca516a06ce2f96d691a0a2dccaee6a529be
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-03-02 11:46:50 -0500

    63. Render Gallery 2

--- Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/examples/Gallery.js
+++ Projects/Gatsby/Gatsby.js_Tutorial_N_Projects_Course/10a-Setup_New_Project/Tutorial/src/examples/Gallery.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
-
+import styled from "styled-components"
 const query = graphql`
   {
     allFile(filter: { extension: { ne: "svg" } }) {
@@ -11,7 +11,8 @@ const query = graphql`
           gatsbyImageData(
             layout: FIXED
             placeholder: BLURRED
-            transformOptions: { grayscale: true }
+            width: 200
+            height: 200
           )
         }
       }
@@ -20,12 +21,35 @@ const query = graphql`
 `
 const Gallery = () => {
   const data = useStaticQuery(query)
-  console.log(data)
+  const nodes = data.allFile.nodes
   return (
-    <div>
-      <h2>Simple Image Gallery</h2>
-    </div>
+    <Wrapper>
+      {nodes.map((image, index) => {
+        const { name } = image
+        return (
+          <article key={index} className="item">
+            <GatsbyImage
+              image={image.childImageSharp.gatsbyImageData}
+              alt="{name}"
+              className="gallery-img"
+            />
+            <p>{name}</p>
+          </article>
+        )
+      })}
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  .item {
+    margin-right: 1rem;
+  }
+  .gallery-img {
+    border-radius: 1rem;
+  }
+`
 
 export default Gallery

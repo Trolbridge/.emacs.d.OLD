75aacdf2e29cfcc89b44e8c4f59d0e9fd8fcbf91
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-03-06 10:28:56 -0500

    JS-Primitive vs Reference

--- Projects/JavaScript/The_JavaScript_Bible_-_Bootcamp_2022/index.js
+++ Projects/JavaScript/The_JavaScript_Bible_-_Bootcamp_2022/index.js
@@ -1,1 +1,9 @@
 document.write("<h2>Hello from external</h2>");
+variableA = {
+  a: 10,
+  b: true,
+};
+copyofA = variableA;
+copyofA.a = 20;
+// variableA.a now equals 20
+document.write(variableA.a);

23326304138ba5da87e6ead9a3b2d6537523ed00
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-04-06 23:11:56 -0400

    Lets get Rusty

---
+++ Projects/Rust/Projects/hello_world/hello_cargo/target/debug/deps/hello_cargo-292e472d240266a9.d
@@ -0,0 +1,5 @@
+/Users/trolbridge/.emacs.d/Projects/Rust/Projects/hello_world/hello_cargo/target/debug/deps/hello_cargo-292e472d240266a9: src/main.rs
+
+/Users/trolbridge/.emacs.d/Projects/Rust/Projects/hello_world/hello_cargo/target/debug/deps/hello_cargo-292e472d240266a9.d: src/main.rs
+
+src/main.rs:
---
+++ Projects/Rust/Projects/hello_world/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/trolbridge/.emacs.d/Projects/Rust/Projects/hello_world/hello_cargo/target/debug/hello_cargo: /Users/trolbridge/.emacs.d/Projects/Rust/Projects/hello_world/hello_cargo/src/main.rs

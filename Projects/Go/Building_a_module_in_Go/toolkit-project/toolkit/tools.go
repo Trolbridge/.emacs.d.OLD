79a957614513c3680b45884d60903fd268646a8d
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-09-09 17:18:03 -0400

    Golang - Build a module (workspaces) Start 9 update 1

--- Projects/Go/Building_a_module_in_Go/toolkit-project/toolkit/tools.go
+++ Projects/Go/Building_a_module_in_Go/toolkit-project/toolkit/tools.go
@@ -11,7 +11,7 @@ type Tools struct {}
 // RandomString returns a string of random characters of length n, using randomStringSource
 // as the source for the strinG
 func (t *Tools) RandomString(n int) string {
-	s, r := map([]rune, n), []rune(randomStringSource)
+	s, r := make([]rune, n), []rune(randomStringSource)
 	for i := range s {
 		p, _ := rand.Prime(rand.Reader, len(r))
 		x, y := p.Uint64(), uint64(len(r))

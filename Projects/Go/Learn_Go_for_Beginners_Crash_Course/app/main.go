5b408c75ade3eaa11f15b924113000795c6daac2
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-09-11 11:03:51 -0400

    Learn Go for Beginners Crash Course - Made an array (grocery list)

--- Projects/Go/Learn_Go_for_Beginners_Crash_Course/app/main.go
+++ Projects/Go/Learn_Go_for_Beginners_Crash_Course/app/main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
-	"fmt"
+	// "bufio"
 	"doctor"
+	"fmt"
+	// "os"
 )
 func main() {
+	// reader := bufio.NewReader(os.Stdin)
+
+
 	whatToSay := doctor.Intro()
 	hi := doctor.Hello
+	b := doctor.B
+	
 
 	// whatToSay = doctor.Intro()
 
 	fmt.Println(whatToSay)
 	fmt.Println(hi)
+	fmt.Println("Grocery List:", b)
+
+	// userInput, _ := reader.ReadString('\n')
+
+	// fmt.Println(userInput)
+
 	
 }
--- Projects/Go/Learn_Go_for_Beginners_Crash_Course/doctor/test.go
+++ Projects/Go/Learn_Go_for_Beginners_Crash_Course/doctor/test.go
@@ -1,3 +1,5 @@
 package doctor
 
 var Hello string = "Fart"
+
+var B = []string{"eggs", "butter", "tomates", "onions", "macadamia nuts", "ham","mushrooms","cheese"}

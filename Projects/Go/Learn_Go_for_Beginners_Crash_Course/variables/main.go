b6c1aaf762cf5504d8b6db8fcf3e926825548d3e
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-09-13 00:30:27 -0400

    Learn Go Lesson 12 2

--- Projects/Go/Learn_Go_for_Beginners_Crash_Course/variables/main.go
+++ Projects/Go/Learn_Go_for_Beginners_Crash_Course/variables/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 
 func main() {
@@ -10,10 +14,15 @@ func main() {
 	var subtraction = 7
 	var answer
 
+	reader := bufio.NewReader(os.Stdin)
+
 	// display a welcome/instructions
 	fmt.Println("Guess the Number Game")
 	fmt.Println("---------------------")
 	fmt.Println("")
+
+	fmt.Println("Think of a number between 1 and 10, press ENTER when ready")
+	reader.ReadString('\n')
 
 
 	// take them through the games

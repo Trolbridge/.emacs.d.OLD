da3fd8f3b0f9489fdaf6e9bb898b1d7a0008a643
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-09-15 20:02:22 -0400

    Learn Go Lesson Crash Course - console-app big mac

--- Projects/Go/Learn_Go_for_Beginners_Crash_Course/console-app/app/main.go
+++ Projects/Go/Learn_Go_for_Beginners_Crash_Course/console-app/app/main.go
@@ -1,9 +1,27 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		fmt.Pr
+		fmt.Print("-> ")
+
+		userInput, _ := reader.ReadString('\n')
+
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "quit" {
+			break
+		} else {
+			fmt.Println(userInput)
+		}
+
 	}
 }

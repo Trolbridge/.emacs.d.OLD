0e5160a11f6c8232ddc73a58013f36530ab0a1c1
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-09-18 21:34:56 -0400

    Learncpp.com Lesson 0.7 Update added semi's

--- Projects/c++/learncpp/lesson_0.7/main.cc
+++ Projects/c++/learncpp/lesson_0.7/main.cc
@@ -3,8 +3,8 @@
 
 int main() {
     std::cout << "Hello BB!" << std::endl;
-    std::cin.clear() // reset any error flags
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n') // ignore any characters in the input buffer until we find a new line
+    std::cin.clear(); // reset any error flags
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find a new line
     std::cin.get();
     return 0;
 }

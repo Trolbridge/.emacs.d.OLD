b9b34ae8558f9ee20cd4bd743f5704df1aec4e5e
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2022-09-23 11:25:34 -0400

    Big update to EMACS CONFIG using Matthew technique

---
+++ custom-set-variables.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(pdf-tools plantuml-mode ox-gfm htmlize toc-org json-mode emmet-mode vue-mode typescript-mode js2-mode web-mode yaml-mode bazel groovy-mode dockerfile-mode docker auctex ess haskell-mode lsp-pyright flycheck-rust rust-mode go-mode modern-cpp-font-lock ccls lsp-java company-box company-tabnine company dap-mode lsp-ui lsp-mode ein iedit evil-nerd-commenter format-all quickrun highlight-indent-guides smartparens dumb-jump flycheck-popup-tip flycheck-posframe flycheck yasnippet-snippets yasnippet projectile magit emojify all-the-icons page-break-lines dashboard doom-modeline doom-themes ibuffer-vc disk-usage exec-path-from-shell term-keys shell-here ace-window discover-my-major undo-tree popup-kill-ring which-key avy crux find-file-in-project counsel amx ivy sudo-edit diminish auto-package-update use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(avy-lead-face ((t (:background "#51afef" :foreground "#870000" :weight bold))))
+ '(css-selector ((t (:inherit default :foreground "#66CCFF"))))
+ '(cursor ((t (:background "BlanchedAlmond"))))
+ '(dashboard-banner-logo-title ((t (:family "Love LetterTW" :height 123))))
+ '(flycheck-posframe-face ((t (:foreground "#98be65"))))
+ '(flycheck-posframe-info-face ((t (:foreground "#98be65"))))
+ '(font-lock-comment-face ((t (:foreground "#828282"))))
+ '(lsp-ui-doc-background ((t (:background nil))))
+ '(lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic)))))
+ '(org-agenda-current-time ((t (:foreground "spring green")))))
